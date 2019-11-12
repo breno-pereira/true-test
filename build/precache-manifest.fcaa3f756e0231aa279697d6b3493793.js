e45cf529d6d6f8ad580d6bee58950d7d6aae1aa3
breno-pereira <breno.pereira@redealumni.com>
2019-11-11 23:26:03 -0300

    fix heroku deploy

---
+++ build/precache-manifest.fcaa3f756e0231aa279697d6b3493793.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c6c582a719c78cc54dcc215bdcf5d6d",
+    "url": "/static/media/cover.9c6c582a.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8b14551c683ab894898c",
+    "url": "/static/js/main.f4c999ea.chunk.js"
+  },
+  {
+    "revision": "9b5d483ceb1474b30186",
+    "url": "/static/js/2.afb5df13.chunk.js"
+  },
+  {
+    "revision": "8b14551c683ab894898c",
+    "url": "/static/css/main.8bbaecd5.chunk.css"
+  },
+  {
+    "revision": "9b5d483ceb1474b30186",
+    "url": "/static/css/2.16d083c2.chunk.css"
+  },
+  {
+    "revision": "a59a999bb839e6186d8670cd70b333ca",
+    "url": "/index.html"
+  }
+];
